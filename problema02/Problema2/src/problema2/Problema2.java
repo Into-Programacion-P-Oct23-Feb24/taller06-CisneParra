b7df8ac25de62b419f0817f19f99c583b6eff405
Cisne Parra <adparra2@utpl.edu.ec>
2023-11-16 16:12:34 -0500

    Cambios del trabajo grupal

---
+++ problema02/Problema2/src/problema2/Problema2.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Problema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double descuento;
+        double total;
+        
+        System.out.println("Ingrese el propietario del vehiculo");
+        String propietario;
+        propietario = entrada.nextLine();
+
+        System.out.println("Ingrese el tipo de vehiculo");
+        String vehiculo;
+        vehiculo = entrada.nextLine();
+        System.out.println("Ingrese el valor de vehiculo");
+        double valor;
+        valor = entrada.nextDouble();
+        vehiculo = vehiculo.toLowerCase();
+        switch (vehiculo) {
+            case "liviano particular":
+            case "liviano":
+            case "particular":
+                descuento = (valor * 0.01) / 100;
+                total = descuento + 2;
+                System.out.printf("Peaje Buena via\nPropeitario: %s\n"
+        + "Tipo: %s\n Valor vehiculo:%.2f\n"
+        + "Peaje:%.2f",propietario,vehiculo,valor,total);
+                break;
+            case "pesado particular":
+            case "pesado":
+            case "grande":
+                descuento = (valor * 0.02) / 100;
+                total = descuento + 2.50;
+                System.out.printf("Peaje Buena via\nPropeitario: %s\n"
+        + "Tipo: %s\n Valor vehiculo:%.2f\n"
+        + "Peaje:%.2f",propietario,vehiculo,valor,total);
+                break;
+            case "taxi":
+
+                descuento = (valor * 0.04) / 100;
+                total = descuento + 1.50;
+                System.out.printf("Peaje Buena via\nPropeitario: %s\n"
+        + "Tipo: %s\n Valor vehiculo:%.2f\n"
+        + "Peaje:%.2f",propietario,vehiculo,valor,total);
+                break;
+            case "bus urbano":
+            case "bus":
+            case "urbano":
+
+                descuento = (valor * 0.05) / 100;
+                total = descuento + 2.20;
+                System.out.printf("Peaje Buena via\nPropeitario: %s\n"
+        + "Tipo: %s\n Valor vehiculo:%.2f\n"
+        + "Peaje:%.2f",propietario,vehiculo,valor,total);
+                break;
+            default:
+                System.out.printf("Datos inespecificados");
+                break;
+
+        }
+
+    }
+}
